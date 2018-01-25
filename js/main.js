a4a7d5c638faa64f49cb2a603ab7de3af9307067
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-25 14:31:40 +0100

    Selection Evnet
    Adding edition selecting event. But stylizing bogues exist because nth-of-type(x) and section switching.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
 var energisante = $('section#recettes div:first-child button.badge:nth-of-type(1)')
 var desalterante = $('section#recettes div:first-child button.badge:nth-of-type(2)')
 var selectionner = $('section#recettes div div article div p')
-
-
-
-
+var btnarticle1 = $('section#recettes div div article:nth-of-type(1) div p')
+var btnarticle2 = $('section#recettes div div article:nth-of-type(2) div p')
+var btnarticle3 = $('section#recettes div div article:nth-of-type(3) div p')
+var article1 = $('section#recettes div div article:nth-of-type(1)')
+var article2 = $('section#recettes div div article:nth-of-type(2)')
+var article3 = $('section#recettes div div article:nth-of-type(3)')
+var section3 = $('section:nth-of-type(3) div div')
 
 
 /*API Google Map*/
@@ -23,13 +26,24 @@ $(function() {
 	/*Disponible vs. Indisponible*/
 	energisante.on('click', function() {
 		selectionner.text('Non disponible')
-	})
+	});
 
 	desalterante.on('click', function() {
 		selectionner.text('Sélectionner')
-	})
+	});
 
+	//if selectionner.text('Sélectionner') == 'Sélectionner' {
+			btnarticle1.on('click', function() { 
+				article1.appendTo(section3)
+			});
+			btnarticle2.on('click', function() { 
+				article2.appendTo(section3)
+			});
+			btnarticle3.on('click', function() { 
+				article3.appendTo(section3)
+			});
 
+	//}
 	
 });
 
